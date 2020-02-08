06012dfd8f6a46afc43b5c95b72cba750a173f90
Konstantin <fandeluk.k@gmail.com>
2020-02-08 10:43:06 +0200

    forma method post

---
+++ src/pages/send.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Форма заявки с сайта</title>
+</head>
+<body>
+<?php
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['name']) and !isset($_POST['email'])){
+ ?> <form action="send.php" method="post">
+<input type="text" name="name" placeholder="Ваше имя" required>
+<input type="text" name="email" placeholder="Ноиер телефона" required>
+<button type="submit" value="Отправить">
+</form> <?php
+} else {
+ //показываем форму
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $name = htmlspecialchars($name);
+ $email = htmlspecialchars($email);
+ $name = urldecode($name);
+ $email = urldecode($email);
+ $name = trim($name);
+ $email = trim($email);
+ if (mail("fandeluk.k@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: fandeluk.k@gmail.com \r\n")){
+ echo "Сообщение успешно отправлено";
+ } else {
+ echo "При отправке сообщения возникли ошибки";
+ }
+}
+?>
+</body>
+</html>

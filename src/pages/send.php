2f2a34429074d37f8bc54e9398ff083f827e127c
Konstantin <fandeluk.k@gmail.com>
2020-02-11 10:23:24 +0200

    fix img

--- src/pages/send.php
+++ src/pages/send.php
@@ -1,32 +1,15 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['name']) and !isset($_POST['email'])){
- ?> <form action="http://note4biz.com/" method="post">
-<input type="text" name="name" placeholder="Ваше имя" required>
-<input type="text" name="email" placeholder="Ноиер телефона" required>
-<button type="submit" value="Отправить">
-</form> <?php
-} else {
- //показываем форму
  $name = $_POST['name'];
- $email = $_POST['email'];
+ $tel = $_POST['phone'];
  $name = htmlspecialchars($name);
- $email = htmlspecialchars($email);
+ $tel = htmlspecialchars($tel);
  $name = urldecode($name);
- $email = urldecode($email);
+ $tel = urldecode($tel);
  $name = trim($name);
- $email = trim($email);
- if (mail("fandeluk.k@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: fandeluk.k@gmail.com \r\n")){
+ $tel = trim($tel);
+ if (mail("fandeluk.k@gmail.com", "Заявка с сайта", "ФИО:".$name.". Телефон: ".$tel ,"From: fandeluk.k@gmail.com \r\n")){
  echo "Сообщение успешно отправлено";
  } else {
  echo "При отправке сообщения возникли ошибки";
  }
-}
 ?>
-</body>
-</html>
